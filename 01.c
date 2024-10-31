e2e95cc7905757928106904666ea54e75834ad10
damgin <ant529@outlook.fr>
2024-10-31 15:26:08 +0100

    v1

--- 01.c
+++ 01.c
@@ -1,33 +1,74 @@
 #include <stdbool.h>
+#include <string.h>
 
-// Définition des capacités spéciales pour chaque classe
+
 typedef struct {
     char nom[30];
-    int puissance;          // Puissance de la capacité
-    int cout_endurance;     // Coût en Endurance pour utiliser la capacité
+    int puissance;
+    int cout_endurance;
 } CapaciteSpeciale;
 
-// Définition de l'équipement pour l'inventaire
 typedef struct {
     char nom[30];
-    int effet_pv;           // Effet de l'objet sur les PV
-    int effet_endurance;    // Effet de l'objet sur l'Endurance
-    int effet_morale;       // Effet de l'objet sur la Morale
+    int effet_pv;
+    int effet_endurance;
+    int effet_morale;
 } Equipement;
 
-// Structure du personnage
 typedef struct {
-    char nom[30];           // Nom du personnage
-    char classe[20];        // Classe : "Soldat", "Érudit" ou "Maraudeur"
-    int pv_max;             // Points de vie maximum
-    int pv_actuel;          // Points de vie actuels
-    int endurance;          // Endurance actuelle
-    int morale;             // Morale actuelle
-    
-    CapaciteSpeciale capacite;  // Capacité spéciale de la classe
-    bool est_leader;            // Détermine si le personnage est le leader
-
-    Equipement inventaire[10];  // Inventaire avec une capacité de 10 objets maximum
-    int nb_objets_inventaire;   // Nombre d'objets dans l'inventaire
-
+    char nom[30];
+    char classe[20];
+    int pv_max;
+    int pv_actuel;
+    int endurance;
+    int morale;
+    CapaciteSpeciale capacite;
+    bool est_leader;
+    Equipement inventaire[10];
+    int nb_objets_inventaire;
 } Personnage;
+
+
+typedef struct {
+    Personnage personnages[3];  
+    int nb_personnages;         
+
+   
+} Equipe;
+
+
+
+int main() {
+    
+    Equipe equipe;
+    equipe.nb_personnages = 3;
+
+    ////////////SOLDAT
+    strcpy(equipe.personnages[0].nom, "Falthes");
+    strcpy(equipe.personnages[0].classe, "Soldat");
+    equipe.personnages[0].pv_max = 100;
+    equipe.personnages[0].pv_actuel = 100;
+    equipe.personnages[0].endurance = 80;
+    equipe.personnages[0].morale = 60;
+    equipe.personnages[0].est_leader = true;
+
+    ////////////ERUDIT
+    strcpy(equipe.personnages[1].nom, "Jean");
+    strcpy(equipe.personnages[1].classe, "Érudit");
+    equipe.personnages[1].pv_max = 70;
+    equipe.personnages[1].pv_actuel = 70;
+    equipe.personnages[1].endurance = 100;
+    equipe.personnages[1].morale = 80;
+    equipe.personnages[1].est_leader = false;
+
+    ////////////MARAUDEUR
+    strcpy(equipe.personnages[2].nom, "Casca");
+    strcpy(equipe.personnages[2].classe, "Maraudeur");
+    equipe.personnages[2].pv_max = 80;
+    equipe.personnages[2].pv_actuel = 80;
+    equipe.personnages[2].endurance = 90;
+    equipe.personnages[2].morale = 50;
+    equipe.personnages[2].est_leader = false;
+
+    return 0;
+}
